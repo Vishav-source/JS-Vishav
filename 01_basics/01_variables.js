91e3f54f99e56b4184b207e7fdcd7fad33fcb579
Vishav-source <vishavbaigra@gmail.com>
2026-09-24 17:37:39 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const id=12345
+let accountpassword="vishav@8458"
+// we don't use var because of scope thing, we only use let variable
+accounttype="sbi_account"
+accountpassword="baigra@8458"
+
+console.table([id,accountpassword,accounttype])
