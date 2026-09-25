bf374d62565782b7a260a253c884fd8599bf6403
Vishav-source <vishavbaigra@gmail.com>
2026-09-25 19:31:25 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let somenumber=44
+// console.log(typeof somenumber)
+// let stringsomenumber=String(somenumber)
+// console.log(typeof stringsomenumber)
+// console.log(stringsomenumber)
+
+
+
+/*let stringsomenumber="44abc"
+console.log(typeof stringsomenumber)
+let somenumber=Number(stringsomenumber)
+console.log(typeof stringsomenumber)
+console.log(somenumber)*/
+
+// "33"-->33
+//"33sgg"-->Nan
+//true--> 1, false-->0
+//""-->true, "hdifh"--->false
+
+
+//*********************Operations*****************
+
+// console.log(2*3);
+// console.log(2+3);
+// console.log(2-3);
+
+
+// console.log("2"+"3")
+// console.log("2"+3)
+// console.log(2+"3")
+// console.log(2+2+"3")
+// console.log("3"+2+2)
+
+
+console.log(true)
+console.log(+true)
+
+console.log(false)
+console.log(+false)
+
+console.log(+"")
+
